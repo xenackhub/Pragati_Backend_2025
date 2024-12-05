ee529a637bce0b103a0510e01294dcf510124f6d
Saran Hiruthik <156305967+hirux06@users.noreply.github.com>
2024-12-05 22:13:43 +0530

    Update envValidator.js

--- config/envValidator.js
+++ config/envValidator.js
@@ -1,28 +1,34 @@
 import validator from "validator";
 
-dotenv.config();
-
 const validateEnv = () => {
     const rules = {
         SERVER_PORT: 'number',
         DB_PWD: 'string',
+        SECRET_KEY: 'string'
     };
-
     const isValid = Object.entries(rules).every(([key, type]) => {
         const value = process.env[key];
-        if (!value || (type === 'number' && !validator.isNumeric(value)) || (type === 'url' && !validator.isURL(value)) || (typeof value !== type)) {
-            console.error(`[ERROR]: Invalid Key: ${key}`);
+        if (!value) {
+            console.error(`[ERROR]: Missing Key: ${key}.`);
             return false;
         }
+        if (type === 'number' && !validator.isNumeric(value)) {
+            console.error(`[ERROR]: Invalid Value for ${key}. Expected Number. Got ${value}.`);
+            return false;
+        }
+        if (type === 'string' && validator.isEmpty(value)) {
+            console.error(`[ERROR]: Invalid Value for ${key}. Expected non-empty String. Got ${value}.`);
+            return false;
+        }
+        
         return true;
     });
-
     if (!isValid) {
       return false;
     }
-
-    console.log('[LOG]: All variables validated.');
+    console.log('[LOG]: All env variables validated.');
     return true;
 };
-
-export {validateEnv}
+export {
+    validateEnv
+}

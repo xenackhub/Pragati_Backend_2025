ce066cda7aae8e6d4bd5bf05d1b6c03f2c328442
SP Saran Dharshan <cb.en.u4aie22045@cb.students.amrita.edu>
2024-12-07 20:06:55 +0530

    Controller functions for signup

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 // helper functions for data validation and consistent response
-import { validateEmail, validatePassword } from "../utilities/dataValidator.js";
+import { validateEmail, validatePassword,validateSignupData  } from "../utilities/dataValidator.js";
 import {
   setResponseOk,
   setResponseBadRequest,
@@ -9,6 +9,7 @@ import {
 
 // authorization module for database access
 import authModule from "../module/authModule.js";
+import { logError } from "../utilities/errorLogger.js";
 
 const authController = {
   /*
@@ -38,6 +39,67 @@ const authController = {
       const response = setResponseInternalError();
       return res.status(response.responseCode).json(response.responseBody);
     }
+  },
+    /*
+  Signup request body
+  {
+      "email": "string",
+      "password": "string",
+      "userName": "string",
+      "rollNumber": "string",
+      "phoneNumber": "string",
+      "collegeName": "string",
+      "collegeCity": "string",
+      "userDepartment": "string",
+      "academicYear": number,
+      "degree": "string",
+      "needAccommodationDay1" : "boolean",
+      "needAccommodationDay2" : "boolean",
+      "needAccommodationDay3" : "boolean",
+      "isAmrita" : "boolean"
+  }
+  */
+  signup: async (req, res) => {
+    const {
+      email,
+      password,
+      userName,
+      rollNumber,
+      phoneNumber,
+      collegeName,
+      collegeCity,
+      userDepartment,
+      academicYear,
+      degree,
+      isAmrita,
+    } = req.body;
+        // Validate input data
+        const validationErrors = validateSignupData(req.body);
+        if (validationErrors != null) {
+          const response = setResponseBadRequest(validationErrors);
+          return res.status(response.responseCode).json(response.responseBody);
+        }
+    
+        try {
+          const response = await authModule.signup({
+            email,
+            password,
+            userName,
+            rollNumber,
+            phoneNumber,
+            collegeName,
+            collegeCity,
+            userDepartment,
+            academicYear,
+            degree,
+            isAmrita,
+          });
+          return res.status(response.responseCode).json(response.responseBody);
+        } catch (err) {
+          logError(err, "authController:signup", "db");
+          const response = setResponseInternalError();
+          return res.status(response.responseCode).json(response.responseBody);
+      }
   },
 };
 

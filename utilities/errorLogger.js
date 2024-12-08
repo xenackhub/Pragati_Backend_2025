d791cf8534efb58d5788fd406b3c9a8aa9187747
Thanus Kumaar <thanuskumaara@gmail.com>
2024-12-08 17:21:03 +0530

    [FIX]: error logger file modified to log mail errors and messages

--- utilities/errorLogger.js
+++ utilities/errorLogger.js
@@ -3,11 +3,19 @@ import { appendFileSync } from "fs";
 const logError = (err, location, type) => {
   const timeStamp = new Date().toLocaleString();
   const errMessage = `[ERROR]: Occured at ${location} \n ${timeStamp} - ${err.message}`;
-  console.error(errMessage);
   if (type == "connection") {
+    console.error(`[ERROR]: Occured at ${location} : \n ${err}`);
     appendFileSync("./logs/connection/poolConnection.log", `${errMessage}\n`);
   } else if (type == "db") {
+    console.error(`[ERROR]: Occured at ${location} : \n ${err}`);
     appendFileSync("./logs/db/dbErrors.log", `${errMessage}\n`);
+  } else if (type == "mailError") {
+    console.error(`[ERROR]: Occured at ${location} : \n ${err}`);
+    appendFileSync("./logs/mailer.log", `${errMessage}\n`);
+  } else if (type == "mailLog") {
+    console.error(`[LOG]: Occured at ${location} : \n ${err}`);
+    const errMessage = `[LOG]: Occured at ${location} \n ${timeStamp} - ${err}`;
+    appendFileSync("./logs/mailer.log", `${errMessage}\n`);
   }
 };
 
--- utilities/mailer/mailer.js
+++ utilities/mailer/mailer.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
-import { appendFileSync } from "fs";
 import { appConfig } from "../../config/config.js";
+import { logError } from "../errorLogger.js";
 
 // Template Imports
 import TEMPLATE_OTP from "./template_otp.js";
@@ -11,44 +11,69 @@ const mailTransporter = nodemailer.createTransport(appConfig.mailer.obj);
 
 // Standard Class defined to avoid Redundancy in Mail Option Initialization.
 class mailOptions {
-    constructor(userEmail, subject, html) {
-        this.from = {
-            name: appConfig.mailer.name,
-            address: appConfig.mailer.obj.auth.user,
-        };
-        this.to = userEmail;
-        this.subject = subject;
-        this.html = html;
-    }
+  constructor(userEmail, subject, html) {
+    this.from = {
+      name: appConfig.mailer.name,
+      address: appConfig.mailer.obj.auth.user,
+    };
+    this.to = userEmail;
+    this.subject = subject;
+    this.html = html;
+  }
 }
 
 const sendMail = async (mailOption, logContext, userEmail) => {
-    mailTransporter.sendMail(mailOption, function (error, info) {
-        const logPrefix = `[${new Date().toLocaleString()} - ${logContext}]`;
-        if (error) {
-            console.log("[ERROR]: Error in Mailer", error);
-            appendFileSync('./logs/mailer.log', `${logPrefix} Mailer - [ERROR]: ${error}\n`);
-        } else {
-            console.log(`[LOG]: ${logContext} Mail sent: ${userEmail}`);
-            appendFileSync('./logs/mailer.log', `${logPrefix} - ${logContext} Mailer - [MESSAGE]: ${logContext} Mail sent: ${mailOption.to}\n`);
-        }
-    });
+  mailTransporter.sendMail(mailOption, function (error, info) {
+    if (error) {
+      logError(error, logContext, "mailError");
+    } else {
+      logError("Mail sent successfully!", logContext, "mailLog");
+    }
+  });
 };
 
 export const sendRegistrationOTP = async (userName, OTP, userEmail) => {
-    const mailSubject = "Pragati 2025 - Account Registration - [OTP Verification]";
-    const registrationMailOption = new mailOptions(userEmail, mailSubject, TEMPLATE_OTP(OTP, userName));
-    await sendMail(registrationMailOption, "Account Registration OTP", userEmail);
-}
+  const mailSubject =
+    "Pragati 2025 - Account Registration - [OTP Verification]";
+  const registrationMailOption = new mailOptions(
+    userEmail,
+    mailSubject,
+    TEMPLATE_OTP(OTP, userName)
+  );
+  await sendMail(registrationMailOption, "Account Registration OTP", userEmail);
+};
 
 export const sendForgotPasswordOtp = async (userName, OTP, userEmail) => {
-    const mailSubject = "Pragati 2025 - Forgot Password - [OTP Verification]";
-    const forgotPassMailOption = new mailOptions(userEmail, mailSubject, TEMPLATE_FORGOT_PASSWORD_OTP(OTP, userName));
-    await sendMail(forgotPassMailOption, "Forgot Password OTP", userEmail);
-}
+  const mailSubject = "Pragati 2025 - Forgot Password - [OTP Verification]";
+  const forgotPassMailOption = new mailOptions(
+    userEmail,
+    mailSubject,
+    TEMPLATE_FORGOT_PASSWORD_OTP(OTP, userName)
+  );
+  await sendMail(forgotPassMailOption, "Forgot Password OTP", userEmail);
+};
 
-export const sendEventRegistrationCompleteOtp = async (userEmail, userName, eventName, transactionId, totalMembers) => {
-    const mailSubject = "Pragati 2025 - Event Registration Mail";
-    const eventRegistrationMailOption = new mailOptions(userEmail, mailSubject, TEMPLATE_EVENT_REGISTRATION_OTP(userName, eventName, transactionId, totalMembers));
-    await sendMail(eventRegistrationMailOption, "Event Registration OTP", userEmail);
-}
+export const sendEventRegistrationCompleteOtp = async (
+  userEmail,
+  userName,
+  eventName,
+  transactionId,
+  totalMembers
+) => {
+  const mailSubject = "Pragati 2025 - Event Registration Mail";
+  const eventRegistrationMailOption = new mailOptions(
+    userEmail,
+    mailSubject,
+    TEMPLATE_EVENT_REGISTRATION_OTP(
+      userName,
+      eventName,
+      transactionId,
+      totalMembers
+    )
+  );
+  await sendMail(
+    eventRegistrationMailOption,
+    "Event Registration OTP",
+    userEmail
+  );
+};

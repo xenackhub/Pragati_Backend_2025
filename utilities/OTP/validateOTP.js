5e7fe3b67b2e42692cc718828b2e178c4d1060f9
Naganathan05 <umaiyalramesh@gmail.com>
2024-12-09 16:26:37 +0530

    Verify User Account Done and validateOTP utility function added.

---
+++ utilities/OTP/validateOTP.js
@@ -0,0 +1,41 @@
+import {
+    setResponseOk,
+    setResponseBadRequest,
+    setResponseTimedOut,
+    setResponseInternalError
+} from "../response.js";
+
+/*
+    responseCode = 400 -> Invalid OTP,
+    responseCode = 408 -> OTP Expiry,
+    responseCode = 500 -> Internal Server Error,
+    responseCode = 200 -> OTP Validated.
+*/
+
+export const validateOTP = async function (userID, OTP, db){
+    try {
+        const [otpRecord] = await db.query(`
+            SELECT * FROM otpTable
+            WHERE userID = ? AND otp = ?`,
+            [userID, OTP]
+        );
+
+        if(otpRecord.length === 0){
+            return setResponseBadRequest("Invalid OTP");
+        } 
+            
+        const isExpired = await db.query(`   
+            DELETE FROM otpTable  
+            WHERE userID = ? AND otp = ? AND expiryTime > NOW()`,     
+            [userID, OTP]
+        );
+
+        if (isExpired[0].affectedRows === 0) {
+            return setResponseTimedOut("OTP Expired. Retry !");
+        } 
+
+        return setResponseOk("Valid OTP");
+    } catch (error) {
+        return setResponseInternalError(error);
+    }
+}

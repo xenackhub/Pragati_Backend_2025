e318fdfd8f3ec37bf811cc469517745cba6eb28c
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2024-12-12 13:26:43 +0530

    Written skeleton for each controller and routes

--- controller/userController.js
+++ controller/userController.js
@@ -1,1 +1,22 @@
-//Created userController
+const userController = {
+    /* 
+        Comment for editOrganiser function
+    */
+    editOrganiser: async (req, res) => {},
+    /* 
+        Comment for removeOrganiser function
+    */
+    removeOrganiser: async (req, res) => {},
+    /* 
+        Comment for addOrganiser function
+    */
+    addOrganiser: async (req, res) => {},
+    /* 
+        Comment for accomodationUpdate function
+    */
+    accomodationUpdate: async (req, res) => {},
+
+};
+
+
+export default userController;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,12 @@
-//Created userRoute.js
+import { Router } from "express";
+import userController from "../controller/userController";
+
+const userRouter = Router();
+
+//Still controllers have to be written, so for now I'm just linking it...
+userRouter.put("/editOrganiser", userController.editOrganiser);
+userRouter.delete("/removeOrganiser",userController.removeOrganiser);
+userRouter.post("/addOrganiser",userController.addOrganiser);
+userRouter.put("/accomodationUpdate",userController.accomodationUpdate);
+
+export default userRouter

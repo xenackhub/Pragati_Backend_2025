f6af664e7652db406b4a69ad4fc8c8a5dced2fb3
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2024-12-12 22:57:25 +0530

    editOrganiser function alone written for review

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,33 @@
+import {setResponseOk,  setResponseBadRequest, setResponseInternalError, } from "../utilities/response.js";
+import userModule from "../module/userModule.js";
+import { logError } from "../utilities/errorLogger.js";
+
 const userController = {
     /* 
-        Comment for editOrganiser function
+        Edit Organiser request body
+        {
+            "organizerID": "integer",
+            "organizerName": "string",
+            "phoneNumber": "string",
+        }
     */
-    editOrganiser: async (req, res) => {},
-    /* 
-        Comment for removeOrganiser function
-    */
+    editOrganizer: async (req, res) => {
+        const { organizerID, organizerName, phoneNumber } = req.body;
+    
+        if (!organizerID || !organizerName || !phoneNumber) {
+            const response = setResponseBadRequest("Invalid or missing organizer data.");
+            return res.status(response.responseCode).json(response.responseBody);
+        }
+    
+        try {
+            const response = await userModule.editOrganizer(organizerID, { organizerName, phoneNumber });
+            return res.status(response.responseCode).json(response.responseBody);
+        } catch (error) {
+            logError(error, "userController:editOrganizer", "db");
+            const response = setResponseInternalError();
+            return res.status(response.responseCode).json(response.responseBody);
+        }
+    },
     removeOrganiser: async (req, res) => {},
     /* 
         Comment for addOrganiser function
--- module/userModule.js
+++ module/userModule.js
@@ -1,1 +1,30 @@
-//Created userModule.js
+import { pragatiDb } from "../db/poolConnection.js";
+import { setResponseOk, setResponseBadRequest, setResponseInternalError } from "../utilities/response.js";
+import { logError } from "../utilities/errorLogger.js";
+
+
+const userModule = {
+    editOrganizer: async function(organizerID, data) {
+      const db = await pragatiDb.promise().getConnection();
+      try {
+        const query = `
+          UPDATE organizerData 
+          SET organizerName = ?, phoneNumber = ?, updatedAt = CURRENT_TIMESTAMP
+          WHERE organizerID = ?`;
+  
+        const [result] = await db.query(query, [data.organizerName, data.phoneNumber, organizerID]);
+        if (result.affectedRows === 0) {
+          return setResponseBadRequest("Organizer not found.");
+        }
+        return setResponseOk("Organizer updated successfully.");
+      } catch (error) {
+        console.log("[ERROR]: Error in Edit Organiser Module: ", err);
+        logError(error, "userModule:editOrganizer", "db");
+        return setResponseInternalError();
+      } finally {
+        db.release();
+      }
+    },
+}
+
+export default userModule;

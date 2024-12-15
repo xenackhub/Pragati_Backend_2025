7fb2246e6393fee856f2862d640cd6b2b34f7353
akshayks13 <akshayks1005@gmail.com>
2024-12-15 16:38:14 +0530

    Resolved merge conflicts

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authRouter from "./authRoute.js";
 import tagRouter from "./tagRoute.js";
+import eventRouter from "./eventRoute.js";
 import organizerRouter from "./organizerRoute.js";
 
 const router = Router();
 
 router.use('/auth',authRouter);
 router.use('/tag', tagRouter); 
+router.use("/event", eventRouter);
 router.use('/org',organizerRouter)
 
 export default router;

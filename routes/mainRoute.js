48c64d04439659028397b66344e1587ef050c181
SP Saran Dharshan <cb.en.u4aie22045@cb.students.amrita.edu>
2024-12-18 23:30:26 +0530

    added adminRoute in mainRoute

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import authRouter from "./authRoute.js";
 import eventRouter from "./eventRoute.js";
 import organizerRouter from "./organizerRoute.js";
+import adminRouter from "./adminRoute.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/event", eventRouter);
-router.use('/org',organizerRouter)
+router.use('/org',organizerRouter);
+router.use('/admin', adminRouter);
+
 
 export default router;

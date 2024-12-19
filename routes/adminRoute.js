52406e47b3d8561b78025b64a9a8d3de10c37025
SP Saran Dharshan <cb.en.u4aie22045@cb.students.amrita.edu>
2024-12-20 00:07:54 +0530

    changed wrong role authorized

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,7 +5,7 @@ import { tokenValidator } from '../middleware/auth/tokenValidator.js';
 
 const adminRouter = Router();
 
-adminRouter.use(tokenValidator("JWT"),authorizeRoles([2]))
+adminRouter.use(tokenValidator("JWT"),authorizeRoles([1]))
 adminRouter.get("/transactions", adminController.getAllTransactions);
 
 export default adminRouter;

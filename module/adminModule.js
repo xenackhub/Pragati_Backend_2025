5034b4b9900f52a96a05ee98e98f368bb4251c88
SP Saran Dharshan <cb.en.u4aie22045@cb.students.amrita.edu>
2024-12-20 00:03:03 +0530

    changes in admin module

--- module/adminModule.js
+++ module/adminModule.js
@@ -1,15 +1,25 @@
-import { transactionsDb } from '../db/poolConnection.js';
+import { transactionsDb, pragatiDb } from '../db/poolConnection.js';
+import { setResponseOk, setResponseInternalError } from "../utilities/response.js";
 import { logError } from '../utilities/errorLogger.js';
 
-export const getAllTransactionsData = () => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM transactionData';
-    transactionsDb.query(query, (err, results) => {
-      if (err) {
-        logError(err, 'getAllTransactionsData (adminModule)', 'db');
-        return reject(err);
-      }
-      resolve(results);
-    });
-  });
+const adminModule = {
+  getAllTransactions: async () => {
+    const db = await transactionsDb.promise().getConnection();
+    try {
+      // Lock table for READ
+      await db.query("LOCK TABLES transactionData READ");
+      const query = "SELECT * FROM transactionData";
+      const [results] = await db.query(query);
+
+      return setResponseOk("All transactions fetched successfully", results);
+    } catch (error) {
+      logError(error, "adminModule:getAllTransactions", "db");
+      return setResponseInternalError();
+    } finally {
+      await db.query("UNLOCK TABLES");
+      db.release();
+    }
+  },
 };
+
+export default adminModule;

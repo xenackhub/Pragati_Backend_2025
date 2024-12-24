f124a9209a205f662c9185d95744475b0a29a6ba
SP Saran Dharshan <cb.en.u4aie22045@cb.students.amrita.edu>
2024-12-24 23:46:20 +0530

    Added edit user account status fucntionality for admins controller

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,9 +1,12 @@
-import { setResponseInternalError } from "../utilities/response.js";
+import { setResponseInternalError, setResponseBadRequest} from "../utilities/response.js";
 import adminModule from "../module/adminModule.js";
 import { logError } from "../utilities/errorLogger.js";
+import { validateEditUserStatusData } from "../utilities/dataValidator/admin.js";
 
 const adminController = {
+
   getAllTransactions : async (req, res) => {
+  
     try {
       const response = await adminModule.getAllTransactions();
       return res.status(response.responseCode).json(response.responseBody);
@@ -13,6 +16,7 @@ const adminController = {
       return res.status(response.responseCode).json(response.responseBody);
     }
   },
+
   getAllRoles: async (req, res) => {
     try {
       const response = await adminModule.getAllRoles();
@@ -23,12 +27,38 @@ const adminController = {
       return res.status(response.responseCode).json(response.responseBody);
     }
   },
+
   getEventWiseAmountGenerated: async (req, res) => {
     try {
       const response = await adminModule.getEventWiseAmountGenerated();
       return res.status(response.responseCode).json(response.responseBody);
     } catch (error) {
       logError(error, "adminController:getEventWiseAmountGenerated", "db");
+      const response = setResponseInternalError();
+      return res.status(response.responseCode).json(response.responseBody);
+    }
+  },
+  changeStatusOfUser: async (req, res) => {
+        /* 
+        Edit accountStatus request body
+        {
+            "studentID": "integer"
+            "accountStatus": "integer"
+        }
+    */
+    const { studentID, accountStatus } = req.body;
+    // Ensure userID is a number, and accountStatus is one of '0', '1', or '2'
+    const validationError = validateEditUserStatusData( studentID, accountStatus);
+    if (validationError !=null) {
+      const response = setResponseBadRequest(validationError);
+      return res.status(response.responseCode).json(response.responseBody);
+    }
+
+    try {
+      const response = await adminModule. editUserAccountStatus(studentID, accountStatus);
+      return res.status(response.responseCode).json(response.responseBody);
+    } catch (error) {
+      logError(error, "adminController:changeStatusOfUser", "db");
       const response = setResponseInternalError();
       return res.status(response.responseCode).json(response.responseBody);
     }

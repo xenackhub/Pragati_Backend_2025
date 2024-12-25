d1aab841c3e429b2144c9277513c72cb152d03de
SP Saran Dharshan <cb.en.u4aie22045@cb.students.amrita.edu>
2024-12-25 14:36:51 +0530

    Added edit user roleID Coltroller Functionality to admin

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,7 +1,7 @@
 import { setResponseInternalError, setResponseBadRequest} from "../utilities/response.js";
 import adminModule from "../module/adminModule.js";
 import { logError } from "../utilities/errorLogger.js";
-import { validateEditUserStatusData } from "../utilities/dataValidator/admin.js";
+import { validateEditUserStatusData,  validateEditUserRoleData} from "../utilities/dataValidator/admin.js";
 
 const adminController = {
 
@@ -59,6 +59,32 @@ const adminController = {
       return res.status(response.responseCode).json(response.responseBody);
     } catch (error) {
       logError(error, "adminController:changeStatusOfUser", "db");
+      const response = setResponseInternalError();
+      return res.status(response.responseCode).json(response.responseBody);
+    }
+  },
+  changeUserRole: async (req, res) => {
+    const {studentID, studentRoleID } = req.body;
+    /* 
+        Edit accountStatus request body
+        {
+            "studentID": "integer"
+            "studentRoleID": "integer"
+        }
+    */
+    // 1) Validate input
+    const validationError = validateEditUserRoleData(studentID, studentRoleID);
+    if (validationError !=null) {
+      const response = setResponseBadRequest(validationError);
+      return res.status(response.responseCode).json(response.responseBody);
+    }
+
+    try {
+      // 2) Call the module
+      const response = await adminModule.updateUserRole(studentID,studentRoleID);
+      return res.status(response.responseCode).json(response.responseBody);
+    } catch (err) {
+      logError(err, "adminController.updateUserRole", "db");
       const response = setResponseInternalError();
       return res.status(response.responseCode).json(response.responseBody);
     }

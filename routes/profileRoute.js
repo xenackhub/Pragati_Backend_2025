6cfd2b185f490badff74d752b0c0b7fd237f21e8
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-01-13 19:48:56 +0530

    Format code.

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,12 +1,8 @@
-
-
-
 import { Router } from 'express';
 import profileController from "../controller/profileController.js";
 import { tokenValidator } from '../middleware/auth/tokenValidator.js';
 
 const profileRouter = Router();
-
 
 profileRouter.get("/:userID(\\d+)",profileController.getUserProfile);
 profileRouter.put("/edit", profileController.editProfile);

90f591ff330807b3dd0d0b95328776a110c4a52d
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-02-12 22:15:50 +0530

    Format code

--- routes/registrationRoute.js
+++ routes/registrationRoute.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import registrationController from "../controller/registrationController.js"
+import registrationController from "../controller/registrationController.js";
 import { tokenValidator } from "../middleware/auth/tokenValidator.js";
 
 const registrationRouter = Router();
 
-registrationRouter.post("/event", tokenValidator("JWT"), registrationController.addRegistration);
+registrationRouter.post(
+    "/event",
+    tokenValidator("JWT"),
+    registrationController.addRegistration,
+);
 
 export default registrationRouter;
